bfb3a45cb72e999a61456abc3e38bfd3ce105d79
TernguFaith <pever.terngu@binghamuni.edu.ng>
2024-12-04 15:06:21 -0800

    Add files via upload

---
+++ optional.py
@@ -0,0 +1,12 @@
+number_list = [3,11,6,7,30,31,6,2,52,63]
+
+index = 0
+while number_list[index:]:
+    index += 1
+
+    number_index = index - 1
+
+for number in range(number_index, -1, -1):
+
+    print(number_list[number])
+     #for reversing without using reverse directly
